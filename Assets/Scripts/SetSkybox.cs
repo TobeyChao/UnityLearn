b2f995c57cac1e2a38fcb6e7eac657293a363b3c
TobeyChao <TobeyChao@qq.com>
2020-08-30 18:44:54 +0800

    删除没有用的shader和材质 提交天空盒测试场景 更新相关脚本

---
+++ Assets/Scripts/SetSkybox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSkybox : MonoBehaviour
+{
+    public Material skybox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = skybox;
+    }
+}
