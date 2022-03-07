08cd3cec22da12cc6bac445fc435599f2379a277
tobeychao <tobeychao@qq.com>
2022-03-07 23:58:41 +0800

    Draw Line DDA

--- Assets/Scripts/DrawLineTest.cs
+++ Assets/Scripts/DrawLineTest.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum DRAW_TYPE
+{
+    DDA,
+    MiddleBresenham,
+    Bresenham
+}
+
 public class DrawLineTest : MonoBehaviour
 {
     public Button mItem;
@@ -15,6 +22,8 @@ public class DrawLineTest : MonoBehaviour
     private GridLayoutGroup layoutGroup;
 
     private int _x1 = -1, _y1 = -1, _x2 = -1, _y2 = -1;
+
+    public DRAW_TYPE mDrawType;
 
     // Start is called before the first frame update
     void Start()
@@ -37,22 +46,41 @@ public class DrawLineTest : MonoBehaviour
         mItem.gameObject.SetActive(false);
     }
 
-    void OnClick(int x, int y)
+    void OnClick(int row, int col)
     {
-        gameObjects[x][y].SetColor(Color.red);
-
         if (_x1 == -1)
         {
-            _x1 = x;
-            _y1 = y;
-            return;
+            _x1 = col;
+            _y1 = row;
         }
-        _x2 = x;
-        _y2 = y;
-        DrawLine(_x1, _y1, _x2, _y2);
+        else if (_x2 == -1)
+        {
+            _x2 = col;
+            _y2 = row;
+            switch (mDrawType)
+            {
+                case DRAW_TYPE.DDA:
+                    DDADrawLine(_x1, _y1, _x2, _y2);
+                    break;
+                case DRAW_TYPE.MiddleBresenham:
+                    break;
+                case DRAW_TYPE.Bresenham:
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            _x1 = col;
+            _y1 = row;
+            Clear();
+        }
+        Debug.Log("OnClick:(" + col + ", " + row + ")");
+        gameObjects[row][col].SetColor(Color.green);
     }
 
-    private void Reset()
+    private void Clear()
     {
         for (int i = 0; i < mHeight; i++)
         {
@@ -61,26 +89,66 @@ public class DrawLineTest : MonoBehaviour
                 gameObjects[i][j].SetColor(Color.white);
             }
         }
+        _x2 = -1;
+        _y2 = -1;
     }
 
-    private void DrawLine(int x1, int y1, int x2, int y2)
+    /// <summary>
+    /// DDA
+    /// 优点：
+    ///     增量算法（每一步都是上一步的x,y值增加一个量）
+    ///     直观、易实现
+    /// 缺点：
+    ///     需要进行浮点数运算；
+    ///     产生一个像素要做两次加法和两次取整运算；
+    ///     运行效率低；
+    ///     取整运算不利于硬件实现。
+    /// </summary>
+    /// <param name="x1"></param>
+    /// <param name="y1"></param>
+    /// <param name="x2"></param>
+    /// <param name="y2"></param>
+    private void DDADrawLine(int x1, int y1, int x2, int y2)
     {
+        int step;
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        if (Math.Abs(dy) > Math.Abs(dx))
+        {
+            step = Math.Abs(y2 - y1);
+        }
+        else
+        {
+            step = Math.Abs(x2 - x1);
+        }
 
+        float incrementX = (float)dx / step;
+        float incrementY = (float)dy / step;
+
+        float x = x1;
+        float y = y1;
+
+        for (int i = 0; i < step; i++)
+        {
+            x += incrementX;
+            y += incrementY;
+            gameObjects[(int)(y + 0.5f)][(int)(x + 0.5f)].SetColor(Color.red);
+        }
     }
 }
 
 public class Item
 {
-    int _x;
-    int _y;
+    int _row;
+    int _col;
     Button _button;
     Image _image;
     Action<int, int> _action;
 
-    public Item(Button button, int x, int y, Action<int, int> action)
+    public Item(Button button, int row, int col, Action<int, int> action)
     {
-        _x = x;
-        _y = y;
+        _row = row;
+        _col = col;
         _button = button;
         _action = action;
         _button.onClick.AddListener(OnClick);
@@ -89,7 +157,7 @@ public class Item
 
     void OnClick()
     {
-        _action(_x, _y);
+        _action(_row, _col);
     }
 
     public void SetColor(Color c)
